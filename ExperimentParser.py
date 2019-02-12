8f7151a2a1a3e0678a4ce8c7cc8f9becd532f5d5
Giovanni De Toni <giovanni.det@gmail.com>
2019-02-12 16:10:47 +0100

    Add automatic experiments (fixes #4).

---
+++ ExperimentParser.py
@@ -0,0 +1,26 @@
+import configparser
+
+class ExperimentParser:
+
+    def __init__(self, args, config_path):
+        self.parser = configparser.ConfigParser()
+        self.parser.read(config_path)
+        self.args = args
+
+    def parse(self):
+
+        self.args["algorithms"] = self.getlist(self.parser["Default"]["algorithms"])
+        self.args["jaccard_tresh"] = self.getlist(self.parser["Default"]["jaccard_tresh"])
+        self.args["database_size"] = self.getlist(self.parser["Default"]["database_size"])
+        self.args["max_tree_size"] = self.getlist(self.parser["Default"]["max_tree_size"])
+        self.args["max_sequence_size"] = self.getlist(self.parser["Default"]["max_sequence_size"])
+
+        return self.args
+
+
+    def getlist(self, option, sep=',', chars=None):
+        """Return a list from a ConfigParser option. By default,
+           split on a comma and strip whitespaces."""
+        return [chunk.strip(chars) for chunk in option.split(sep)]
+
+
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import pickle
 from ComplexPrefixSpan.Builder import Builder
 from ComplexPrefixSpan.ComplexPrefixSpan import ComplexPrefixSpan
 
+from ExperimentParser import ExperimentParser
+
 from pymining import itemmining
 
 if __name__ == "__main__":
@@ -28,51 +30,109 @@ if __name__ == "__main__":
     parser.add_argument('--use_frequent_itemset', default=False, action="store_true", help="Use frequent itemset (to compare).")
     parser.add_argument('--jaccard_tresh', default=1.0, type=float, help="Jaccard Similarity treshold")
     parser.add_argument("--output_dir", default="./results/", type=str, help="Output dir for the results")
+    parser.add_argument("--experiments", default="", type=str, help="Run the experiments using the config.")
 
     # Parse the command line arguments
     args = parser.parse_args()
 
-    # Build the dataset
-    print("[*] Building the dataset.")
-    if args.structure_type == "synthea":
-        dataset = Builder.create_from_synthea(args.dataset_path)
-    else:
-        dataset = Builder.create_dataset(args.dataset_path)
-
-    # Build a new dataset using hashes instead of the plain sequences
-    if args.hash:
-        print("[*] Building the hash version of the dataset.")
-        dataset, hashDict = Builder.create_hash_dataset(dataset)
-
-    # Generate the name of the output file
-    if not args.use_frequent_itemset:
-        output_result = os.path.splitext(os.path.basename(args.dataset_path))[0]+"_"+str(int(args.jaccard_tresh*100))\
-                        +"_"+str(args.min_support)+"_"+str(args.max_length_sequence)+".db"
-    else:
-        output_result = os.path.splitext(os.path.basename(args.dataset_path))[0] + "_frequentitems_"\
-                        + str(args.min_support) + ".db"
-
-    if not args.use_frequent_itemset:
-        # Find complex sequences
-        print("[*] Executing Prefix Span algorithm on {} cores".format(args.cores))
-
-        finder = ComplexPrefixSpan(dataset, int(args.cores), args.jaccard_tresh)
-        if not args.profile_execution:
-            result = finder.compute(args.min_support, args.max_length_sequence, args.iterative)
-            result[1].sort()
-
-            print(result[1])
+    if args.experiments == "":
+        # Build the dataset
+        print("[*] Building the dataset.")
+        if args.structure_type == "synthea":
+            dataset = Builder.create_from_synthea(args.dataset_path)
         else:
-            result = profile.run("finder.compute(args.min_support, args.max_length_sequence, args.iterative)", "prefix_span.stat")
-            p = pstats.Stats('prefix_span.stat')
-            p.strip_dirs().sort_stats(-1).print_stats()
-    else:
-        print("[*] Execution Frequent Itemset algorithm.")
-        relim_input = itemmining.get_relim_input(dataset)
-        result = itemmining.relim(relim_input, min_support=args.min_support)
-        print(result[1])
+            dataset = Builder.create_dataset(args.dataset_path)
 
-    # Save the file to disk
-    with open(args.output_dir+output_result, "wb") as f:
-        pickle.dump(result[1], f)
+        # Build a new dataset using hashes instead of the plain sequences
+        if args.hash:
+            print("[*] Building the hash version of the dataset.")
+            dataset, hashDict = Builder.create_hash_dataset(dataset)
+
+        # Generate the name of the output file
+        if not args.use_frequent_itemset:
+            output_result = os.path.splitext(os.path.basename(args.dataset_path))[0]+"_"+str(int(args.jaccard_tresh*100))\
+                            +"_"+str(args.min_support)+"_"+str(args.max_length_sequence)+".db"
+        else:
+            output_result = os.path.splitext(os.path.basename(args.dataset_path))[0] + "_frequentitems_"\
+                            + str(args.min_support) + ".db"
+
+        if not args.use_frequent_itemset:
+            # Find complex sequences
+            print("[*] Executing Prefix Span algorithm on {} cores".format(args.cores))
+
+            finder = ComplexPrefixSpan(dataset, int(args.cores), args.jaccard_tresh)
+            if not args.profile_execution:
+                result = finder.compute(args.min_support, args.max_length_sequence, args.iterative)
+                result[1].sort()
+
+                print(result[1])
+            else:
+                result = profile.run("finder.compute(args.min_support, args.max_length_sequence, args.iterative)", "prefix_span.stat")
+                p = pstats.Stats('prefix_span.stat')
+                p.strip_dirs().sort_stats(-1).print_stats()
+        else:
+            print("[*] Execution Frequent Itemset algorithm.")
+            relim_input = itemmining.get_relim_input(dataset)
+            result = itemmining.relim(relim_input, min_support=args.min_support)
+            print(result[1])
+
+        # Save the file to disk
+        with open(args.output_dir+output_result, "wb") as f:
+            pickle.dump(result[1], f)
+    else:
+
+        exp_parser = ExperimentParser(dict(), "./experiments.conf")
+        args_e = exp_parser.parse()
+
+        for alg in args_e["algorithms"]:
+            for d_size in args_e["database_size"]:
+                for m_tree in args_e["max_tree_size"]:
+                    for m_seq in args_e["max_sequence_size"]:
+                        for j in args_e["jaccard_tresh"]:
+
+                            # Use frequent itemset only with equality
+                            if alg == "frequent" and float(j) != 1.0:
+                                continue
+
+                            # Database name
+                            dataset_name = "./datasets/dataset_"+d_size+"_"+m_tree+"_"+m_seq+".db"
+
+                            # Build the dataset
+                            print("[*] Building the dataset.")
+                            if args.structure_type == "synthea":
+                                dataset = Builder.create_from_synthea(args.dataset_path)
+                            else:
+                                dataset = Builder.create_dataset(dataset_name)
+
+                            # Build a new dataset using hashes instead of the plain sequences
+                            if args.hash:
+                                print("[*] Building the hash version of the dataset.")
+                                dataset, hashDict = Builder.create_hash_dataset(dataset)
+
+                            # Generate the name of the output file
+                            if not alg == "frequent":
+                                output_result = os.path.splitext(os.path.basename(dataset_name))[0] + "_" + str(
+                                    int(args.jaccard_tresh * 100)) \
+                                                + "_" + str(args.min_support) + "_" + str(args.max_length_sequence) + ".db"
+                            else:
+                                output_result = os.path.splitext(os.path.basename(dataset_name))[0] + "_frequentitems_" \
+                                                + str(args.min_support) + ".db"
+
+                            if not alg == "frequent":
+                                # Find complex sequences
+                                print("[*] Executing Prefix Span algorithm on {} cores on dataset {}".format(args.cores, dataset_name))
+
+                                finder = ComplexPrefixSpan(dataset, int(args.cores), float(j))
+                                result = finder.compute(args.min_support, args.max_length_sequence, args.iterative)
+                                result[1].sort()
+                                print(result[1])
+                            else:
+                                print("[*] Execution Frequent Itemset algorithm on dataset {}".format(dataset_name))
+                                relim_input = itemmining.get_relim_input(dataset)
+                                result = itemmining.relim(relim_input, min_support=args.min_support)
+                                print(result[1])
+
+                            # Save the file to disk
+                            with open(args.output_dir + output_result, "wb") as f:
+                                pickle.dump(result[1], f)
 
